6e2395be7f395032de490b33516b9bcae2cb40ca
simonakomarovska <simonak399@gmail.com>
2024-10-02 17:19:09 +0200

    fixed login test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -14,7 +14,7 @@ describe('Successful login', () => {
   // Click on 'Signup / Login' button
   cy.get('a[href="/login"]').should('contain.text', 'Signup / Login').and('be.visible').click();
   // Verify 'Login to your account' is visible
-  cy.get('h2').should('contain.text', 'Login to your account').and('be.visible');
+  cy.get('.login-form h2').should('contain.text', 'Login to your account').and('be.visible');
   // Enter correct email address and password
   cy.get('input[data-qa="login-email"]').type(user.email).should('have.value', user.email).and('be.visible');
   cy.get('input[data-qa="login-password"]').type(user.password).should('have.value', user.password).and('be.visible');
@@ -38,16 +38,18 @@ describe('Failed login', () => {
   });
 
   it('Login User with incorrect email and password', () => {
-   cy.navigateToHomePageAndLogin();
-   // Verify 'Login to your account' is visible
-   cy.get('h2').should('contain.text', 'Login to your account').and('be.visible');
-   // Enter incorrect email address and password
-   cy.get('input[data-qa="login-email"]').type('incorrectEmail@gmail.com').should('have.value', 'incorrectEmail@gmail.com').and('be.visible');
-   cy.get('input[data-qa="login-password"]').type('incorrectPassword').should('have.value', 'incorrectPassword').and('be.visible');
-   // Click 'login' button
-   cy.get('button[data-qa="login-button"]').should('contain.text', 'Login').and('be.visible').click();
-   // Verify error 'Your email or password is incorrect!' is visible
-   cy.get('form p').should('contain.text', 'Your email or password is incorrect!').and('be.visible');
+    cy.navigateToHomePage();
+    // Click on 'Signup / Login' button
+    cy.get('a[href="/login"]').should('contain.text', 'Signup / Login').and('be.visible').click();
+    // Verify 'Login to your account' is visible
+    cy.get('.login-form h2').should('contain.text', 'Login to your account').and('be.visible');
+    // Enter incorrect email address and password
+    cy.get('input[data-qa="login-email"]').type('incorrectEmail@gmail.com').should('have.value', 'incorrectEmail@gmail.com').and('be.visible');
+    cy.get('input[data-qa="login-password"]').type('incorrectPassword').should('have.value', 'incorrectPassword').and('be.visible');
+    // Click 'login' button
+    cy.get('button[data-qa="login-button"]').should('contain.text', 'Login').and('be.visible').click();
+    // Verify error 'Your email or password is incorrect!' is visible
+    cy.get('form p').should('contain.text', 'Your email or password is incorrect!').and('be.visible');
   })
 });
 
