cbf60b3e30b7b34faaca2175d240a15c103e904d
simonakomarovska <simonak399@gmail.com>
2024-10-02 20:35:22 +0200

    fixed selector

--- cypress/e2e/subscription.cy.js
+++ cypress/e2e/subscription.cy.js
@@ -1,14 +1,18 @@
 describe('Subscription', () => {
 
   it('Verify subscription in home page', () => {
+  // Navigate to url & verify that home page is visible successfully
   cy.navigateToHomePage();
+  // Subscribe pipeline
   cy.subscribeToNewsletter();
 });
 
   it('Verify subscription in cart page', () => {
+  // Navigate to url & verify that home page is visible successfully
   cy.navigateToHomePage();
   // Click 'Cart' button
   cy.get('a[href="/view_cart"]').first().should('contain.text', ' Cart').and('be.visible').click();
+  // Subscribe pipeline
   cy.subscribeToNewsletter();
   })
 })
